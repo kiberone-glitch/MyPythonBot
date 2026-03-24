2fab140f5c1d09c71c9dbaac03b6df16fffd1a8e
Egor <aanonimyc12@gmail.com>
2026-03-24 18:55:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,44 +1,59 @@
 import telebot
 import time
-from telebot import types 
+from telebot import types
 
 
-
-
-TOKEN = " "
+TOKEN = "8646730344:AAG7-qJe63NYM0vQGySW92Ag_Te2r6OIoTk"
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.InlineKeyboardMarkup ()
-    btn = types.InlineKeyboardButton ("О боте",callback_data="about")
-    btn2 = types.InlineKeyboardButton ("Помощь",callback_data="help")
-    btn3 = types.InlineKeyboardButton ("Поговорить с ботом", callback_data="talk")
-    btn4 = types.InlineKeyboardButton ("Перезапуск", callback_data="start")
+    btn = types.InlineKeyboardButton ("О боте",callback_data="about",style="primary")
+    btn2 = types.InlineKeyboardButton ("Помощь",callback_data="help",style="primary")
+    btn3 = types.InlineKeyboardButton ("Поговорить с ботом", callback_data="talk",style="primary")
+    btn4 = types.InlineKeyboardButton ("Перезапуск", callback_data="start",style="primary")
     markup.add (btn,btn2)
     markup.add (btn3)
     markup.add (btn4)
     bot.send_chat_action (message.chat.id, 'typing')
     time.sleep (2)
-    bot.send_message(message.chat.id, "Привет, это бот Егора",reply_markup=markup)
+    bot.send_message(message.chat.id, "Привет, это бот Егора",reply_markup=markup) 
+
+
 
 @bot.callback_query_handler (func=lambda call2: True)
 def callback2 (call2):
     if call2.data == "about":
-        bot.send_chat_action (call2.message.chat.id, 'typing')
-        time.sleep (2)
-        bot.send_message (call2.message.chat.id, "Это бот Егора, написан на Python ")
+        markup  = types.InlineKeyboardMarkup ()
+        btn = types.InlineKeyboardButton ("Назад",callback_data="start",style="success")
+        markup.add (btn)
+        bot.edit_message_text(
+        "Это бот Егора, написан на Python",
+        call2.message.chat.id,
+        call2.message.message_id,
+        reply_markup=markup
+        )
     elif call2.data == "help":
         bot.send_chat_action (call2.message.chat.id, 'typing')
         time.sleep (2)
-        bot.send_message (call2.message.chat.id, "Этот бот был сделан Егором 14.03.26. Бот был написан на python. Хостится на Railway. Также есть репозиторий на Github.")
+        bot.send_message (
+             call2.message.chat.id,
+             'Этот бот был сделан Егором 14.03.26. Бот был написан на python. Хостится на Railway. Также есть репозиторий на <a href="https://github.com/kiberone-glitch/MyPythonBot">Github</a>',
+             parse_mode="HTML"
+        )
+        
     elif call2.data == "talk":
         bot.send_chat_action (call2.message.chat.id, 'typing')
         time.sleep (2)
-        bot.send_message (call2.message.chat.id, "Привет ✌")
+    
+        bot.send_message (call2.message.chat.id, "Привет ✌")    
     elif call2.data == "start":
-        start (call2.message)   
+        start (call2.message)
+
+
+          
 
 @bot.message_handler(content_types=['text'])
 def chat(message):
@@ -57,6 +72,9 @@ def chat(message):
         bot.send_chat_action (message.chat.id, 'typing')
         time.sleep (2)
         bot.send_message (message.chat.id, "Прости, я не понимаю тебя :( ")
+
+
+
 
                      
 
